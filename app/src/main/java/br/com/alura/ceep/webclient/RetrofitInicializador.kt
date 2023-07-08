d75cb52b06c7d570dab7cb72e85dc2774d6c05b3
EnriqueIzel2 <eleaoizel@gmail.com>
2023-07-08 19:29:34 -0400

    feat: create configuration for service

--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -1,10 +1,13 @@
 package br.com.alura.ceep.webclient
 
+import br.com.alura.ceep.webclient.services.NotaService
 import retrofit2.Retrofit
 
 class RetrofitInicializador {
 
-  val retrofit = Retrofit.Builder()
+  private val retrofit = Retrofit.Builder()
     .baseUrl("http://localhost:8080")
     .build()
+
+  val notaService = retrofit.create(NotaService::class.java)
 }
