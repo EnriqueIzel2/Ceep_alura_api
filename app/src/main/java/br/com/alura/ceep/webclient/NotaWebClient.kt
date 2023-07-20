1c4d8a31bcbe9d5c9e2235a7ebe851cf48acf62a
EnriqueIzel2 <eleaoizel@gmail.com>
2023-07-19 20:12:38 -0400

    feat: add new behavior with try/catch

--- app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -1,13 +1,23 @@
 package br.com.alura.ceep.webclient
 
+import android.util.Log
 import br.com.alura.ceep.model.Nota
+
+private const val TAG = "NotaWebClient"
 
 class NotaWebClient {
 
-  suspend fun buscaTodas(): List<Nota> {
-    val notasResposta = RetrofitInicializador().notaService.buscaTodas()
-    return notasResposta.map { notaResposta ->
-      notaResposta.nota
+  private val notaService = RetrofitInicializador().notaService
+
+  suspend fun buscaTodas(): List<Nota>? {
+    return try {
+      val notasResposta = notaService.buscaTodas()
+      return notasResposta.map { notaResposta ->
+        notaResposta.nota
+      }
+    } catch (e: Exception) {
+      Log.e(TAG, "buscaTodas: ", e)
+      null
     }
   }
 }
