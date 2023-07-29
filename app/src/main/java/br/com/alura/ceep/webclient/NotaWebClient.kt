d60e5ca2fe24951695ea84ae722c80b2b3e7e074
Enrique Izel <eleaoizel@gmail.com>
2023-07-29 14:37:49 -0400

    feat: crete new function to save note

--- app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -2,6 +2,7 @@ package br.com.alura.ceep.webclient
 
 import android.util.Log
 import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.model.NotaRequisicao
 
 private const val TAG = "NotaWebClient"
 
@@ -18,6 +19,26 @@ class NotaWebClient {
     } catch (e: Exception) {
       Log.e(TAG, "buscaTodas: ", e)
       null
+    }
+  }
+
+  suspend fun salva(nota: Nota) {
+    try {
+      val resposta = notaService.salva(
+        nota.id,
+        NotaRequisicao(
+          titulo = nota.titulo,
+          descricao = nota.descricao,
+          imagem = nota.imagem
+        )
+      )
+      if (resposta.isSuccessful) {
+        Log.i(TAG, "salva: Nota salva com sucesso")
+      } else {
+        Log.i(TAG, "salva: Nota não foi salva com sucesso")
+      }
+    } catch (e: Exception) {
+      Log.e(TAG, "salva: falha ao salvar", e)
     }
   }
 }
