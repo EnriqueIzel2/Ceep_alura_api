f24e940bedca9694b97f736a78f8828fe372e57a
Enrique Izel <eleaoizel@gmail.com>
2023-08-03 20:00:31 -0400

    feat: create a new way to remove nota

--- app/src/main/java/br/com/alura/ceep/repository/NotaRepository.kt
+++ app/src/main/java/br/com/alura/ceep/repository/NotaRepository.kt
@@ -26,7 +26,10 @@ class NotaRepository(private val dao: NotaDao, private val webClient: NotaWebCli
   }
 
   suspend fun remove(id: String) {
-    dao.remove(id)
+    dao.desativa(id)
+    if (webClient.remove(id)) {
+      dao.remove(id)
+    }
   }
 
   suspend fun salva(nota: Nota) {
@@ -38,6 +41,10 @@ class NotaRepository(private val dao: NotaDao, private val webClient: NotaWebCli
   }
 
   suspend fun sincroniza() {
+    val notasDesativadas = dao.buscaDesativadas().first()
+    notasDesativadas.forEach { notaDesativada ->
+      remove(notaDesativada.id)
+    }
     val notasNaoSincronizadas = dao.buscaNaoSincronizadas().first()
     notasNaoSincronizadas.forEach { notaNaoSincronizada ->
       salva(notaNaoSincronizada)
